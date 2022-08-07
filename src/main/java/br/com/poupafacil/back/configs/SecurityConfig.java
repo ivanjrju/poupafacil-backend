14840ce88bfbaafdf735a141d2321a92d234a17a
ivanjrju <juniorivandias@gmail.com>
2022-08-07 14:51:04 -0300

    adicionando logs

--- src/main/java/br/com/poupafacil/back/configs/SecurityConfig.java
+++ src/main/java/br/com/poupafacil/back/configs/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-//        .csrf().disable()
+        .csrf().disable()
 		.cors()
 		.and()
         .authorizeRequests()
