c62acd66cca506e8ef6dc2806bd1aa0a90dab643
ivanjrju <juniorivandias@gmail.com>
2022-08-07 14:19:46 -0300

    alterando cors

--- src/main/java/br/com/poupafacil/back/configs/CorsConfig.java
+++ src/main/java/br/com/poupafacil/back/configs/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig extends WebMvcConfigurerAdapter    {
 
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("*");
+		registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
     }
 	
 }
