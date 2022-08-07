c23a28d7bd99a2bf74cc2d1224a69a8d83fde023
ivanjrju <juniorivandias@gmail.com>
2022-08-07 14:00:32 -0300

    ajustando o cors no spring security

--- src/main/java/br/com/poupafacil/back/configs/SecurityConfig.java
+++ src/main/java/br/com/poupafacil/back/configs/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-        .csrf().disable()
+//        .csrf().disable()
         .authorizeRequests()
             .antMatchers("/api/pessoas/**")
                 .hasAnyRole("USER")
